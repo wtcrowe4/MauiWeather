bcf21640c34a5e8e2349ace535338cd12be38978
Thomas Crowe <wcrowe@g.clemson.edu>
2023-06-25 15:25:17 -0400

    Data from api on main page

--- WeatherPage.xaml.cs
+++ WeatherPage.xaml.cs
@@ -1,3 +1,6 @@
+using MauiWeather.Services;
+using System.Diagnostics;
+
 namespace MauiWeather;
 
 public partial class WeatherPage : ContentPage
@@ -5,5 +8,15 @@ public partial class WeatherPage : ContentPage
 	public WeatherPage()
 	{
 		InitializeComponent();
+		OnAppearing();
+	}
+
+	protected async override void OnAppearing()
+	{
+		base.OnAppearing();
+		var result = await APIService.GetWeatherByLatLong(47.6829, -122.1209);
+		CityLabel.Text = result.name;
+		WeatherLabel.Text = result.weather[0].main;
+		Debug.WriteLine(result);
 	}
 }
