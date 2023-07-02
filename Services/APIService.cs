7c6db25fc846b66ddbef8e1505009154e69e38f7
Thomas Crowe <wcrowe@g.clemson.edu>
2023-07-02 15:36:47 -0400

    Push API service without key

---
+++ Services/APIService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+using System.Diagnostics;
+using MauiWeather.Models;
+using Newtonsoft.Json;
+
+namespace MauiWeather.Services
+{
+    public static class APIService
+    {
+        //Get current weather by city
+        public static async Task<Root> GetWeather(string city)
+        {
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://open-weather13.p.rapidapi.com/city/{city}"),
+                Headers =
+                    {
+                        { "X-RapidAPI-Key", "" },
+                        { "X-RapidAPI-Host", "open-weather13.p.rapidapi.com" },
+                    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<Root>(body);
+            }
+        }
+
+        //Get current weather by lat/long
+        public static async Task<Root> GetWeatherByLatLong(double latitude, double longitude)
+        {
+            //Convert to strings
+            string lat = latitude.ToString();
+            string lon = longitude.ToString();
+            Root Data;
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://open-weather13.p.rapidapi.com/city/latlon/{lat}/{lon}"),
+                Headers =
+                    {
+                        { "X-RapidAPI-Key", "" },
+                        { "X-RapidAPI-Host", "open-weather13.p.rapidapi.com" },
+                    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                //need to convert temperatures to F
+                Data = JsonConvert.DeserializeObject<Root>(body);
+                Data.main.temp = Data.main.temp * 9 / 5 - 459.67;
+                return Data;
+            }
+        
+
+        }
+
+        //Get 5 day forecast
+        public static async Task<FRoot> GetWeatherForecast(double latitude, double longitude)
+        {
+            string lat = latitude.ToString();
+            string lon = longitude.ToString();
+            FRoot Data;
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://open-weather13.p.rapidapi.com/city/fivedaysforcast/{lat}/{lon}"),
+                Headers =
+                {
+                        { "X-RapidAPI-Key", "" },
+                        { "X-RapidAPI-Host", "open-weather13.p.rapidapi.com" },
+                    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Data = JsonConvert.DeserializeObject<FRoot>(body);
+                return Data;
+            }
+        }
+    }
+}
