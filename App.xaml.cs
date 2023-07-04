0b646badd7d96911b966314115c8bd64f31b27c6
Thomas Crowe <wcrowe@g.clemson.edu>
2023-07-04 13:30:55 -0400

    Started on a welcome page

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,7 +5,14 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new WeatherPage();
+		VersionTracking.Track();
+		if (VersionTracking.IsFirstLaunchEver == true)
+		{
+			MainPage = new WelcomePage();
+		}
+		else
+		{
+			MainPage = new WeatherPage();
+		}
 	}
 }
---
+++ WelcomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiWeather
+{
+    public partial class WelcomePage : ContentPage
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        public void WelcomeButton_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new WeatherPage();
+        }
+    }
+}
